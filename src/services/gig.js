a453b483b288a392c84b97e7e5ad74c5054dcb56
re1220 <reut.saido@gmail.com>
2024-09-22 18:52:27 +0300

    add demo date

---
+++ src/services/gig.js
@@ -0,0 +1,372 @@
+
+const gig =[ {
+  _id: "i101",
+  title: "I will design your logo",
+  price: 12.16,
+  owner: {
+    "_id": "u101",
+    "fullname": "Dudu Da",
+    "imgUrl": "url",
+    "level": "basic/premium",
+    "rate": 4
+  },
+  daysToMake: 3,
+  description: "Make unique logo...",
+  avgResponseTime: 1,
+  loc: "Ghana",
+  imgUrls: [""],
+  tags: [
+    "Arts And Crafts", "Logo Design"
+  ],
+  likedByUsers: ['mini-user'],
+  reviews: [
+    {
+      "id": "002",
+      "txt": "Did an amazing work",
+      "rate": 4,
+      "by": {
+        "_id": "u102",
+        "fullname": "user2",
+        "imgUrl": "/img/img2.jpg"
+      }
+    },
+    {
+      "id": "001",
+      "txt": "Perfect",
+      "rate": 5,
+      "by": {
+        "_id": "u103",
+        "fullname": "user3",
+        "imgUrl": "/img/img3.jpg"
+      }
+    }
+
+  ], 
+},
+{
+  _id: "i102",
+  title: "I will create minimalist logo design for your business",
+  price: 10,
+  owner: {
+    "_id": "u102",
+    "fullname": "Nancy Car",
+    "imgUrl": "url",
+    "level": "basic",
+    "rate": 5
+  },
+  daysToMake: 5,
+  description: "YOU ARE HERE! It simply means that you are having a good eye for the designs :)",
+  avgResponseTime: 1,
+  loc: "Ghana",
+  imgUrls: [""],
+  tags: [
+    "Logo Design", "wordpress"
+  ],
+  likedByUsers: ['mini-user'],
+  reviews: [
+    {
+      "id": "madeId",
+      "txt": "Amazing work..",
+      "rate": 4,
+      "by": {
+        "_id": "u102",
+        "fullname": "user2",
+        "imgUrl": "/img/img4.jpg"
+      }
+    }
+  ], 
+},
+{
+  _id: "i103",
+  title: "I will deI will web design and build a responsive modern wordpress websitesign your logo",
+  price: 15,
+  owner: {
+    "_id": "u101",
+    "fullname": "user2",
+    "imgUrl": "url",
+    "level": "premium",
+    "rate": 5
+  },
+  daysToMake: 2,
+  description: "Hi, I'm Dinos and I lead a small team of WordPress experts. We specialize in modern results-driven informational WordPress websites. Now based in Greece and previously in Miami (5 years) and London before that (14 years).",
+  avgResponseTime: 1,
+  loc: "Ghana",
+  imgUrls: [""],
+  tags: [
+    "wordpress"
+  ],
+  likedByUsers: ['mini-user'],
+  reviews: [
+    {
+      "id": "002",
+      "txt": "Did an amazing work",
+      "rate": 4,
+      "by": {
+        "_id": "u102",
+        "fullname": "user2",
+        "imgUrl": "/img/img2.jpg"
+      }
+    },
+    {
+      "id": "001",
+      "txt": "Perfect",
+      "rate": 5,
+      "by": {
+        "_id": "u103",
+        "fullname": "user3",
+        "imgUrl": "/img/img3.jpg"
+      }
+    }
+
+  ], 
+},
+
+{
+  _id: "i105",
+  title: "I will edit your resume using my recruiting expertise",
+  price: 10,
+  owner: {
+    "_id": "u101",
+    "fullname": "Alex F",
+    "imgUrl": "url",
+    "level": "basic",
+    "rate": 3
+  },
+  daysToMake: 3,
+  description: "As a recruiter, I've reviewed thousands of resumes",
+  avgResponseTime: 6,
+  loc: "Ghana",
+  imgUrls: [""],
+  tags: [
+    "writing-translation"
+  ],
+  likedByUsers: ['mini-user'],
+  reviews: [
+    {
+      "id": "user1",
+      "txt": "Did an amazing work",
+      "rate": 4,
+      "by": {
+        "_id": "u102",
+        "fullname": "user2",
+        "imgUrl": "/img/img2.jpg"
+      }
+    },
+    {
+      "id": "001",
+      "txt": "Perfect",
+      "rate": 5,
+      "by": {
+        "_id": "u103",
+        "fullname": "user3",
+        "imgUrl": "/img/img3.jpg"
+      }
+    }
+
+  ], 
+},
+{
+  _id: "i106",
+  title: "I will do usa youtube video promotion for organic growth",
+  price: 12.16,
+  owner: {
+    "_id": "u101",
+    "fullname": "Ron A",
+    "imgUrl": "url",
+    "level": "premium",
+    "rate": 2
+  },
+  daysToMake: 3,
+  description: " I'm a GOOGLE LLC Certified & Award Winner Digital Marketer with 5+years of experience in video marketing and promotion.",
+  avgResponseTime: 5,
+  loc: "Ghana",
+  imgUrls: [""],
+  tags: [
+    "video-animation"
+  ],
+  likedByUsers: ['mini-user'],
+  reviews: [
+    {
+      "id": "002",
+      "txt": "Amazing work!!!",
+      "rate": 4,
+      "by": {
+        "_id": "u102",
+        "fullname": "user2",
+        "imgUrl": "/img/img2.jpg"
+      }
+    },
+    {
+      "id": "001",
+      "txt": "Perfect",
+      "rate": 5,
+      "by": {
+        "_id": "u103",
+        "fullname": "user3",
+        "imgUrl": "/img/img3.jpg"
+      }
+    }
+
+  ], 
+},
+{
+  _id: "i107",
+  title: "I will prepare and e file usa tax returns for personal, llc, c corp, s corp",
+  price: 20,
+  owner: {
+    "_id": "u101",
+    "fullname": "David",
+    "imgUrl": "url",
+    "level": "premium",
+    "rate": 5
+  },
+  daysToMake: 3,
+  description: "As an experienced tax specialist, I am dedicated to providing top-notch tax return services tailored to meet your unique needs..",
+  avgResponseTime: 2,
+  loc: "Ghana",
+  imgUrls: [""],
+  tags: [
+    "finance"
+  ],
+  likedByUsers: ['mini-user'],
+  reviews: [
+    {
+      "id": "002",
+      "txt": "Did an amazing work",
+      "rate": 4,
+      "by": {
+        "_id": "u102",
+        "fullname": "user2",
+        "imgUrl": "/img/img2.jpg"
+      }
+    },
+    {
+      "id": "001",
+      "txt": "Perfect",
+      "rate": 5,
+      "by": {
+        "_id": "u103",
+        "fullname": "user3",
+        "imgUrl": "/img/img3.jpg"
+      }
+    }
+
+  ], 
+},
+
+]
+
+
+const orders = [
+  {
+    _id: "o1225",
+    buyer: "mini-user",
+    seller: "mini-user",
+    gig: {
+      _id: "i101",
+      name: "Design Logo",
+      imgUrl: "",
+      price: 20
+    },
+    status: "approved"
+  },
+  {
+    _id: "o1224",
+    buyer: "mini-user",
+    seller: "mini-user",
+    gig: {
+      _id: "i102",
+      name: "wordpress",
+      imgUrl: "",
+      price: 40
+    },
+    status: "pending"
+  },
+  {
+    _id: "o1226",
+    buyer: "mini-user",
+    seller: "mini-user",
+    gig: {
+      _id: "i104",
+      name: "Design Logo",
+      imgUrl: "",
+      price: 200
+    },
+    status: "rejected"
+  }
+]
+
+
+const users = [
+  {
+    _id: "u101",
+    fullname: "User 1",
+    imgUrl: "/img/img1.jpg",
+    username: "user1",
+    password: "secret",
+    level: "basic/premium",
+  },
+  {
+    _id: "u102",
+    fullname: "User 2",
+    imgUrl: "/img/img1.jpg",
+    username: "user2",
+    password: "secret",
+    level: "basic/premium",
+  },
+  {
+    _id: "u103",
+    fullname: "User 3.",
+    imgUrl: "/img/img1.jpg",
+    username: "user3",
+    password: "secret",
+    level: "basic/premium",
+  },
+]
+
+
+
+
+
+const allTags = [
+  "logo-design",
+  "wordpress",
+  "online-marketing",
+  "artisitic",
+  "video-animation",
+  "writing-translation",
+  "programming-tech",
+  "music-audio",
+  "business",
+  "finance",
+  "photography"
+
+]
+// function isPrimaryTag(tag) {
+//   return allTags.slice(0, 3).includes(tag)
+// }
+
+
+// HomePage
+//  list of gigs with link to gig-details
+
+// Gig Details
+// <pre>JSON, slowly improve
+
+// UserDetails 
+//  basic info
+//  orderedGigs => orderService.query({userId: 'u101'})
+//  ownedGigs => gigService.query({ownerId: 'u103'})
+
+// GigEdit - make it super easy to add Gig for development
+
+// GigList, GigOrder
+// Order, confirm Order
+// Lastly: GigExplore, Filtering
+
+
+function loadApp() {
+  socketService.on(SOCKET_EVENT_ORDER_ADDED, (order) => {
+    showSuccessMsg(`Another order was just made, check it out ${order.gig._id}`)
+  })
+}
